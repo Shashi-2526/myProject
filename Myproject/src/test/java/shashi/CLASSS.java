6a003a8cda68d2155110f152296bb3665611447c
hp <hp@LAPTOP-MPMJTA5B>
2025-04-17 13:31:31 +0530

    Shashi commit

---
+++ Myproject/src/test/java/shashi/CLASSS.java
@@ -0,0 +1,12 @@
+package shashi;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CLASSS {
+@Test
+public void hiBoys() {
+	Reporter.log("Hello all well ",true);
+}
+
+}
